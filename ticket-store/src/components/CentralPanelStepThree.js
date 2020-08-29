7804b98de9f72eea8854d85cdc1504b807736fdf
JenHsuan <ofalpha@gmail.com>
2020-08-29 14:41:49 +0800

    add the page 3

--- ticket-store/src/components/CentralPanelStepThree.js
+++ ticket-store/src/components/CentralPanelStepThree.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    selectShopIdState,
+    selectNameState,
+    selectColorState
+  } from '../components/states/states'
+import { 
+    PanelStyle,
+    TitleStyle,
+    PanelItemStyle,
+    ColorCircleBarStyle
+} from '../components/styles/Style'
+import { 
+    setCurrentPage
+} from './actions/ticketAction';
 
 const CentralPanelStepThree = () => {
+    const disPatch = useDispatch();
+    const shopId = useSelector(selectShopIdState);
+    const name = useSelector(selectNameState);
+    const color = useSelector(selectColorState);
+    const [res, setRes] = useState('')
+    useEffect(() => {
+        setRes(JSON.stringify({
+            shopId: shopId,
+            name: name,
+            color: color
+        }));
+        const timer = setTimeout(() => {
+            disPatch(setCurrentPage(0))
+        }, 3000);
+        return () => clearTimeout(timer);
+    },[]);
+
     return (
-        <div>
-            
-        </div>
+        <PanelStyle variant='centerStepTwo'>
+            {res}
+        </PanelStyle>
     )
 }
 
--- ticket-store/src/components/SubPanel.js
+++ ticket-store/src/components/SubPanel.js
@@ -14,26 +14,36 @@ import { useDispatch, useSelector } from 'react-redux';
   
 const SubPanel = () => {
     const currentPage = useSelector(selectPageState);
+    const switchPage = page => {
+        switch(page) {
+          case 0:   
+            return (
+                <PanelStyle variant='sub'>
+                    <NextButton/>
+                </PanelStyle>
+            );
+          case 1:   
+            return (
+                <PanelStyle variant='subStepTwo'>
+                <TitleStyle variant='sub'>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                         小提醒
+                </TitleStyle>
+                <ul>
+                    <li>發送費會每日結算前一天實際發送張數、並在下期請款時收取</li>
+                    <li>折抵費與折抵金額，會依實際消費折抵金額收取</li>
+                </ul>
+                <BackButton/>
+                <NextButton/>
+                </PanelStyle>
+            );
+          default:
+            return <Fragment/>;
+        }
+      }
     return (
     <Fragment>
-    {currentPage == 0 ? (
-        <PanelStyle variant='sub'>
-            <NextButton/>
-        </PanelStyle>
-    ) : (
-        <PanelStyle variant='subStepTwo'>
-            <TitleStyle variant='sub'>
-                <FontAwesomeIcon icon={faInfoCircle} />
-                小提醒
-            </TitleStyle>
-            <ul>
-                <li>發送費會每日結算前一天實際發送張數、並在下期請款時收取</li>
-                <li>折抵費與折抵金額，會依實際消費折抵金額收取</li>
-            </ul>
-            <BackButton/>
-            <NextButton/>
-        </PanelStyle>
-    ) }
+    {switchPage(currentPage)}
     </Fragment>
   )
 }
